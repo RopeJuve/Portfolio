2c789e30b83c8e805d6cfd89228c547027ff86ff
RopeJuve <rope.s@hotmail.com>
2023-06-16 19:50:27 +0200

    hero section

---
+++ components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef } from 'react';
+import styles from './Button.module.css'
+import classNames from 'classnames'
+
+interface ButtonCustomProps extends ComponentPropsWithoutRef<"button">{
+    text:string,
+    variant: string,
+}
+
+const Button = ({text, variant, ...props}:ButtonCustomProps) => {
+
+    const btnClasses = classNames(styles.btn, {
+        [styles.primary]:variant === 'primary',
+        [styles.secondary]: variant === "secondary"
+      });
+  
+
+  return (
+    <button className={btnClasses} {...props}>
+        {text}
+    </button>
+  )
+}
+
+export default Button
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,15 @@
+import Button from "../Button/Button";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hallo, I'm Robert Shterjov Frontend Web Developer</h1>
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1>Hallo, I'm Robert Shterjov Frontend Web Developer</h1>
+        <h2>Transforming Ideas into Beautifully Responsive Websites</h2>
+      </div>
+      <Button text="contact me" variant="primary" />
+      <Button text="download cv" variant="secondary" />
     </div>
   );
 };
