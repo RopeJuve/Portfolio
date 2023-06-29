acbd0321d1959292f6d210ed44f569f569f7f676
RopeJuve <rope.s@hotmail.com>
2023-06-29 16:39:20 +0200

    Json Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About/About";
 import Contact from "@/components/Contact/Contact";
-import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import ProjectsCards from "@/components/ProjectsCards/ProjectsCards";
 import Skills from "@/components/Skills/Skills";
---
+++ data.ts
@@ -0,0 +1,38 @@
+const data = {
+  header: "Hallo,I'm Robert Shterjov Frontend Web Developer",
+  subtitle: "Transforming Ideas into Beautifully Responsive Websites",
+  profileImg: "./images/profile1.png",
+  skills: [
+    { skillIcon: "", skillName: "html" },
+    { skillIcon: "", skillName: "java script" },
+    { skillIcon: "", skillName: "sass" },
+    { skillIcon: "", skillName: "react" },
+    { skillIcon: "", skillName: "git" },
+    { skillIcon: "", skillName: "next" },
+  ],
+
+  aboutMe: [
+    "Hello, I'm Robert Shterjov, a frontend developer based in Germany. I specialize in creating captivating user experiences through beautifully responsive websites using React. With expertise in React, CSS, and JavaScript, I stay up-to-date with the latest trends and technologies.",
+    "Collaboration is key to my process. I work closely with clients and creative teams to bring their vision to life. Communication, attention to detail, and delivering high-quality results are my priorities.",
+    "Let's work together to create a web experience that engages and inspires. Whether you need a stunning website or an interactive web application built with React, I'm here to turn your ideas into reality. Get in touch, and let's make something exceptional!",
+  ],
+  projects: [
+    {
+      title: "AUDIOPHILE E-COMMERCE WEBSITE",
+      description:
+        "Multi-page Figma conversion made using a Frontend Mentor design files. Responsive across devices.",
+      techTitle: "MADE WITH",
+      tech: [
+        { techIcon: "", techName: "React" },
+        { techIcon: "", techName: "Java Script" },
+        { techIcon: "", techName: "Sass" },
+        { techIcon: "", techName: "Redux" },
+      ],
+      projectImg: "./images/Laptop.png"
+    },
+  ],
+
+  contactMe:
+    "Get in touch with me to discuss your project, ask any questions, or simply say hello.I'm here and eager to connect! Whether you have a web development project in mind, need assistance with frontend design, or have inquiries about my services, feel free to reach out using the contact information provided below. I value open communication and prompt responses, so rest assured that I'll get back to you as soon as possible.Let's collaborate and bring your ideas to life. I look forward to hearing from you and being a part of your web development journey!",
+  footerName: "© 2023 ROBERT SHTERJOV",
+};
