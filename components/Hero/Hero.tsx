ac8b652b179f559e8247a673b73208c3d1c0ae48
RopeJuve <rope.s@hotmail.com>
2024-09-10 23:38:42 +0200

    CV

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = ({ title, subTitle, profileImage }: HeaderProps) => {
               usedAs="link"
               text="download cv"
               variant="secondaryLink"
-              href="./assets/RobertShterjovCV06.24.pdf"
+              href="./assets/RobertShterjovCV09_24.pdf"
             />
           </div>
           <Social />
