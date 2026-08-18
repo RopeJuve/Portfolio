0ec051570611ab545bd3af366d9709abc5b9a24b
RopeJuve <rope.s@hotmail.com>
2026-08-18 22:31:41 +0200

    Update Footer component to use an image logo and modify .gitignore
    Replaces the existing Logo component in the Footer with an image link to the logo, enhancing accessibility with an aria-label. Additionally, updates the .gitignore file to include new directories and files for documentation and development tools.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "../Logo";
+
 import Social from "../Social/Social";
 import NavMenuLinks from "../NavMenuLinks/NavMenuLinks";
 
@@ -6,7 +6,13 @@ const Footer = () => {
   return (
     <div className="bg-frame mt-section pt-6">
       <div className="max-w-container mx-auto px-6 md:px-10 xl:px-0">
-        <Logo className="-ml-2 mb-6 w-[6.25rem] h-[6.25rem] text-primary" />
+      <a href="/" aria-label="Home">
+          <img
+            src="/Logo.png"
+            alt="Logo"
+            className="h-6 md:h-7 w-auto"
+          />
+        </a>
         <div className="flex flex-col xl:flex-row xl:justify-between xl:items-center">
           <NavMenuLinks variant="nav-footer" />
           <Social />
